1fc0ecccc260817ce0c2c6469b0e63a6f18cb77f
Denilson Semedo <dstavares00@gmail.com>
2020-01-10 13:12:26 -0100

    Alteração na disposição do menu e algumas funcionalidades a mais

--- SO.bat
+++ SO.bat
@@ -2,39 +2,43 @@
 pause > nul
 ::inicialização
 :inicio
-start /b gplay Microsoft.Mp3>nul 2>nul
+set /a som=0
+if %som%==0 (
+    ::Reproduz o ficheiro .mp3 que se encontra na mesma pasta que o codigo
+    start /b gplay Microsoft.mp3>nul 2>nul
+)
 sleep 400                                                                                         
-echo                                     ```....```                                                     
-echo                               `.-:/++++++++++++/:-.                                                
-echo                             .+++++++++++++++++++++++:.                                             
-echo                             /+++++++++++++++++++++++++`                                            
-echo                            .+++++++++++++++++++++++++-   .:.                                       
-echo                            /+++++++++++++++++++++++++`   /++/:-.`            `..-:/                
-echo                           -+++++++++++++++++++++++++.   -++++++++//::---::///+++++-                
-echo                          `+++++++++++++++++++++++++/   `+++++++++++++++++++++++++/                 
-echo                          :+++++++++++++++++++++++++.   :+++++++++++++++++++++++++.                 
-echo                         .+++++++++++++++++++++++++:   .+++++++++++++++++++++++++:                  
-echo                         /+++++++++++++++++++++++++`   /+++++++++++++++++++++++++`                  
-echo                        -+++++++++++++++++++++++++-   -+++++++++++++++++++++++++-                   
-echo                       `/++///:-..`````..-:/+++++/   `/++++++++++++++++++++++++/`                   
-echo                       .-.``               ``-:/+.   :+++++++++++++++++++++++++.                    
-echo                         ...-:///+++//::-.`    `.   `+++++++++++++++++++++++++/                     
-echo                      -:/+oooooooooooooooo+/:.`     ./++++++++++++++++++++++++.                     
-echo                     .oooooooooooooooooooooooo+-     `.:/+++++++++++++++++//:.                      
-echo                     +oooooooooooooooooooooooo+.`      ``.-::////////::-.``                         
-echo                    -ooooooooooooooooooooooooo:   .::-.                    .:                       
-echo                   `+oooooooooooooooooooooooo+`   :::::::--.::::::::...---::-                        
-echo                   :ooooooooooooooooooooooooo-   .:::::::::::::::::::::::::`                        
-echo                  `ooooooooooooooooooooooooo+   `:::::::::::::::::::::::::-                         
-echo                  /ooooooooooooooooooooooooo.   -:::::::::::::::::::::::::`                         
-echo                 .ooooooooooooooooooooooooo/   `:::::::::::::::::::::::::.                          
-echo                 /ooooooooooooooooooooooooo`   -:::::::::::::::::::::::::`                          
-echo                -oooooo++++////++ooooooooo:   .:::::::::::::::::::::::::.                           
-echo                +/:-..``         `.-/+ooo+`   :::::::::::::::::::::::::-                            
-echo                                      .-/-   .:::::::::::::::::::::::::`                            
-echo                                            `:::::::::::::::::::::::::-                             
-echo                                             `.-:::::::::::::::::::::-`                             
-echo                                                `..-:::::::::::--..`
+echo                                         ```....```                                                     
+echo                                   `.-:/++++++++++++/:-.                                                
+echo                                 .+++++++++++++++++++++++:.                                             
+echo                                 /+++++++++++++++++++++++++`                                            
+echo                                .+++++++++++++++++++++++++-   .:.                                       
+echo                                /+++++++++++++++++++++++++`   /++/:-.`            `..-:/                
+echo                               -+++++++++++++++++++++++++.   -++++++++//::---::///+++++-                
+echo                              `+++++++++++++++++++++++++/   `+++++++++++++++++++++++++/                 
+echo                              :+++++++++++++++++++++++++.   :+++++++++++++++++++++++++.                 
+echo                             .+++++++++++++++++++++++++:   .+++++++++++++++++++++++++:                  
+echo                             /+++++++++++++++++++++++++`   /+++++++++++++++++++++++++`                  
+echo                            -+++++++++++++++++++++++++-   -+++++++++++++++++++++++++-                   
+echo                           `/++///:-..`````..-:/+++++/   `/++++++++++++++++++++++++/`                   
+echo                           .-.``               ``-:/+.   :+++++++++++++++++++++++++.                    
+echo                             ...-:///+++//::-.`    `.   `+++++++++++++++++++++++++/                     
+echo                          -:/+oooooooooooooooo+/:.`     ./++++++++++++++++++++++++.                     
+echo                         .oooooooooooooooooooooooo+-     `.:/+++++++++++++++++//:.                      
+echo                         +oooooooooooooooooooooooo+.`      ``.-::////////::-.``                         
+echo                        -ooooooooooooooooooooooooo:   .::-.                    .:                       
+echo                       `+oooooooooooooooooooooooo+`   :::::::--.::::::::...---::-                        
+echo                       :ooooooooooooooooooooooooo-   .:::::::::::::::::::::::::`                        
+echo                      `ooooooooooooooooooooooooo+   `:::::::::::::::::::::::::-                         
+echo                      /ooooooooooooooooooooooooo.   -:::::::::::::::::::::::::`                         
+echo                     .ooooooooooooooooooooooooo/   `:::::::::::::::::::::::::.                          
+echo                     /ooooooooooooooooooooooooo`   -:::::::::::::::::::::::::`                          
+echo                    -oooooo++++////++ooooooooo:   .:::::::::::::::::::::::::.                           
+echo                    +/:-..``         `.-/+ooo+`   :::::::::::::::::::::::::-                            
+echo                                          .-/-   .:::::::::::::::::::::::::`                            
+echo                                                `:::::::::::::::::::::::::-                             
+echo                                                 `.-:::::::::::::::::::::-`                             
+echo                                                   ´´ `..-:::::::::::--..`
 ::codigo para fazer o efeito de imagen piscando                                
 for /l %%i in (1 1 5) do (
 color 07
@@ -45,13 +49,16 @@ color 07
 ::#############################################
 :menu
 cls
-echo Menu
-echo 1 - Gestao de processo
-echo 2 - Gestao de memoria 
-echo 3 - Terminal/Shell
-echo 4 - Definicoes(*)
-echo 5 - Sair
-set /p op=
+echo         _________________________
+echo                   MENU
+echo         -------------------------
+echo         [1] Gestao de processo
+echo         [2] Gestao de memoria 
+echo         [3] Terminal/Shell
+echo         [4] Definicoes(*)
+echo         _________
+echo         [5] Sair
+set /p op=         *
 if %op% == 1 (
     goto :gesprocessos
 )
@@ -62,54 +69,46 @@ if %op% == 3 (
    goto :terminal 
 )
 if %op% == 4 (
-	if %y% == 1 (
-		goto :def
-	) else (
-		start /b gplay wrong.mp3>nul 2>nul
-		echo.
-		echo ERRO!!!!!!
-		echo Nao tens Permissao do Acesso!!
-		call :pisca
-		pause > nul
-		goto menu
-	)
-    
+		goto :def  
 )
 if %op% == 5 (
     exit    
 )
-if %op% == 5 (
-	goto def
-)
 if %op% gtr 5 (
+    if %som%==0 (
     start /b gplay wrong.mp3>nul 2>nul
+    )
 	call :pisca
     goto :menu    
 )
 if %op% lss 1 (
+    if %som%==0 (
     start /b gplay wrong.mp3>nul 2>nul
+    )
 	call :pisca
     goto :menu
 )
 ::Gestao de Processos
 :gesprocessos
 cls
-echo Gestao de processo
-echo 1 - Processos ativos
-echo 2 - Suspender Processos 
-echo 3 - Continuar um processo
-echo 4 - Matar um Processos
-echo 5 - Voltar para o menu principal
-set /p op1= 
+echo         _____________________________
+echo              GESTAO DE PROCESSOS
+echo         -----------------------------
+echo         [1] Processos ativos
+echo         [2] Suspender Processos 
+echo         [3] Continuar um processo
+echo         [4] Matar um Processos
+echo         __________
+echo         [5] Voltar
+set /p op1=         * 
 if %op1% == 1 (
-
-    goto :gesprocessos
-=======
+    ::Apresenta uma lista de processos ativos e depois encerra o processo inserido pelo utilizador
     TASKLIST /FI "STATUS EQ RUNNING"
     pause
-    goto :meges
+    goto :gesprocessos
 )
 if %op1% == 4 (
+    ::Apresenta uma lista de processos ativos e depois encerra o processo inserido pelo utilizador
     TASKLIST /FI "STATUS EQ RUNNING"
     set /p x=Qual processo desejas matar? 
 	set /a x1=%x%
@@ -121,26 +120,32 @@ if %op1% == 5 (
     goto :menu
 )
 if %op1% gtr 5 (
-	start /b gplay wrong.mp3>nul 2>nul
+	if %som%==0 (
+    start /b gplay wrong.mp3>nul 2>nul
+    )
 	call :pisca
     goto :gesprocessos
 )
 if %op1% lss 1 (
+    if %som%==0 (
     start /b gplay wrong.mp3>nul 2>nul
+    )
 	call :pisca
     goto :gesprocessos
 )
 :gesmemoria
-=======
 ::Gestão de memória
 :meme
 cls
-echo Gestao de memoria
-echo 1 - total de memoria
-echo 2 - Memoria em uso 
-echo 3 - Memoria Disponivel
-echo 4 - Voltar para o menu principal
-set /p op2= 
+echo         _________________________
+echo             GESTAO DE MEMORIA
+echo         -------------------------
+echo         [1] Total de memoria
+echo         [2] Memoria em uso 
+echo         [3] Memoria Disponivel
+echo         ___________
+echo         [4] Voltar 
+set /p op2=         * 
 
 if %op2% == 1 (
     cls
@@ -158,56 +163,67 @@ if %op2% == 4 (
     goto :menu
 )
 if %op2% gtr 4 (
+    if %som%==0 (
     start /b gplay wrong.mp3>nul 2>nul
+    )
 	call :pisca
     goto :gesmemoria
 )
 if %op2% lss 1 (
+    if %som%==0 (
     start /b gplay wrong.mp3>nul 2>nul
+    )
 	call :pisca
     goto :gesmemoria
 )
 ::Memoria Total
 :memoriatotal
+echo         _________________________
+echo             GESTAO DE MEMORIA
+echo         -------------------------
+echo.
+::Utilizamos o ciclo for pra extrair os dados necessarios no comando "Systeminfo"
 for /f "tokens=4,5 delims=, " %%i in ('systeminfo.exe ^| find "Total Physical Memory"') do set RAM_SIZE=%%i%%j
-echo Memoria total: %RAM_SIZE%
+echo         [Memoria total]: %RAM_SIZE%
 pause > nul
 goto :gesmemoria
 ::Memoria Em uso
-:memoriaemuso
+:memoriadisponivel
+echo         _________________________
+echo             GESTAO DE MEMORIA
+echo         -------------------------
+echo.
 for /f "tokens=4,5 delims=, " %%i in ('systeminfo.exe ^| find "Available Physical Memory:"') do set RAM_DIS=%%i%%j
-echo Memoria disponivel: %RAM_DIS%
+echo         [Memoria disponivel]: %RAM_DIS%
 pause > nul
 goto :gesmemoria
 ::Memoria Disponivel
-:memoriadisponivel
+:memoriaemuso
+echo         _________________________
+echo             GESTAO DE MEMORIA
+echo         -------------------------
+echo.
 for /f "tokens=4,5 delims=, " %%i in ('systeminfo.exe ^| find "Total Physical Memory"') do set RAM_SIZE=%%i%%j
 for /f "tokens=4,5 delims=, " %%i in ('systeminfo.exe ^| find "Available Physical Memory:"') do set RAM_DIS=%%i%%j
 set /a RAM_USE=RAM_SIZE-RAM_DIS
-echo Memoria em uso: %RAM_USE%
+echo         [Memoria em uso]: %RAM_USE%
 pause > nul
 goto :gesmemoria
 ::Terminal
 :terminal
     cls
-    echo 1 - Powershell(*)
-    echo 2 - Terminal do cmd
-    echo 3 - Voltar para o menu principal
-    set /p opp=
+    echo         _____________________
+    echo               TERMINAIS
+    echo         ---------------------
+    echo         [1] Powershell(*)
+    echo         [2] Terminal do cmd
+    echo         ___________
+    echo         [3] Voltar
+    set /p opp=         *
     if %opp% == 1 (
-		if %y% == 1 (
-			cls
-			powershell
-			goto :terminal
-		) else (
-			start /b gplay wrong.mp3>nul 2>nul
-			echo.
-			echo ERRO!!!!!!
-			echo Nao tens Permissao do Acesso!!
-			call :pisca
-			pause > nul
-			goto 
-		) 
+        cls
+		powershell
+		goto :terminal
     )
     if %opp% == 2 (
         cls
@@ -218,25 +234,40 @@ goto :gesmemoria
         goto :menu
     )
     if %opp% lss 1 (
+        if %som%==0 (
         start /b gplay wrong.mp3>nul 2>nul
+        )
 	    call :pisca
         goto :terminal
     )
     if %opp% gtr 3 (
+        if %som%==0 (
         start /b gplay wrong.mp3>nul 2>nul
+        )
 	    call :pisca
         goto :terminal
     )
 ::Definicões onde somente o administrador tem acesso
 :def
 cls
-echo DEFINICOES
-echo 1 - Trocar Password
-echo 2 - Mudar Cor
-echo 3 - Voltar
-set /p opd=
+echo         ______________________
+echo              DEFINICOES
+echo         ----------------------
+echo         [1] Trocar Password
+echo         [2] Mudar Cor
+if %som% == 0 (
+    echo         [3] Desativar som    
+)
+if %som% == 1 (
+    echo         [3] Ativar som   
+)
+echo         ___________ 
+echo         [4] Voltar
+set /p opd=         *
 if %opd% lss 1 (
-	start /b gplay wrong.mp3>nul 2>nul
+	if %som%==0 (
+    start /b gplay wrong.mp3>nul 2>nul
+    )
 	call :pisca
     goto :def	
 )
@@ -247,10 +278,21 @@ if %opd% == 2 (
 goto cor
 ) 
 if %opd% == 3 (
+    if %som%==1 (
+        set /a som=0
+    )
+    if %som%==0 (
+        set /a som=1
+    )
+    goto def
+) 
+if %opd% == 4 (
 goto menu
 )
-if %opd% gtr 3 (
-	start /b gplay wrong.mp3>nul 2>nul
+if %opd% gtr 4 (
+	if %som%==0 (
+    start /b gplay wrong.mp3>nul 2>nul
+    )
 	call :pisca
     goto :def
 	
@@ -264,5 +306,43 @@ color 0C
 sleep 100)
 color 0A
 exit /b
-    
-
+:cor
+cls
+echo         _________________
+echo               MODOS
+echo         -----------------
+echo         [1] Padrao
+echo         [2] Modo Nocturno
+echo         [3] Modo Hacker
+echo         ___________
+echo         [4] Voltar
+set /p opcor=         *
+if %opcor% == 1 (
+color 70
+goto cor
+)
+if %opcor% == 2 (
+color 07
+goto cor
+)
+if %opcor% == 3 (
+color 0a
+goto cor
+)
+if %opcor% == 4 (
+goto def
+)
+if %opcor% lss 1(
+	if %som%==0 (
+    start /b gplay wrong.mp3>nul 2>nul
+    )
+	call :pisca
+    goto :cor
+)
+if %opcor% gtr 4 (
+    if %som%==0 (
+    start /b gplay wrong.mp3>nul 2>nul
+    )
+	call :pisca
+    goto :cor
+)
